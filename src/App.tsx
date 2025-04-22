d2616a3267bf1cf1fc7cf57d9978f462fb16a629
Maurolezcano81 <maurolezcano111@gmail.com>
2025-04-22 09:41:56 -0300

    Start hero

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import Hero from "./components/Hero"
+
+function App() {
+
+  return (
+    <main className="relative flex flex-col items-center justify-center px-5 mx-auto overflow-hidden bg-black-100 sm:px-10">
+      <div className="w-full max-w-7xl">
+        <Hero />
+      </div>
+    </main>
+  )
+}
+
+export default App
