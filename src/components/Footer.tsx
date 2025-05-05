38c8bf161e2b07ad2e9398668a98f0d1b6c47fa0
Maurolezcano81 <maurolezcano111@gmail.com>
2025-05-05 08:07:41 -0300

    fix mt

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -33,7 +33,7 @@ const Footer = () => {
 
             <div className='flex m-16 md:flex-row flex-col justify-between items-center'>
                 <p className='md:text-base text-sm md:font-normal font-light'>Copyright © 2025 Mauro Lezcano</p>
-                <div className='flex items-center md:gap-3 gap-6'>
+                <div className='flex items-center md:gap-3 gap-6 mt-2 md:mt-0'>
                     {socialMedia.map((profile) => (
                         <a href={profile.link} target='_BLANK' key={profile.id} className='w-10 h-10 cursor-pointer flex justify-center items-center backdrop-filter backdrop-blur-lg saturate-180 bg-opacity-75 bg-black-200 rounded-lg border border-black-300'>
                             <img src={profile.img} alt={profile.img} width={20} height={20} />
